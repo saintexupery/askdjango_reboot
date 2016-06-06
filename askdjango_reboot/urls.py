0ef0e0cdf22ea598c770ab04bd5a313b2304d72d
Younggi Min <younggimin91@gmail.com>
2016-06-06 18:01:38 +0900

    blog앱에 index view 추가

--- askdjango_reboot/urls.py
+++ askdjango_reboot/urls.py
@@ -14,13 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf import settings
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.shortcuts import redirect
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^blog/', include('blog.urls')),
+    url(r'^$', lambda request: redirect('/blog/')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
