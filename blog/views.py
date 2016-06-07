f633f0af318c7d9ffb535b1b22c45954316a5c1c
Younggi Min <younggimin91@gmail.com>
2016-06-07 15:55:10 +0900

    blog.post_list뷰에서 Post목록 노출

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from .models import Post
 
 def index(request):
-    return render(request, 'blog/index.html')
+    post_list = Post.objects.all()
+    return render(request, 'blog/index.html', {
+        'post_list': post_list,
+    })
