b1f86fc5974ca57ede6f0c466f72e13dafb02418
Younggi Min <younggimin91@gmail.com>
2016-06-07 17:17:43 +0900

    로그인 뷰 적용

--- askdjango_reboot/settings.py
+++ askdjango_reboot/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'account',
     'blog',
 ]
 
@@ -124,4 +125,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+LOGIN_REDIRECT_URL = '/'
+
 
