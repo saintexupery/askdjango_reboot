9d0fba61e28dce78e9b833771d9b8e77955a1bad
Younggi Min <younggimin91@gmail.com>
2016-06-07 16:52:53 +0900

    blog 댓글 쓰기 기능 구현

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,19 @@
 from django.db import models
+from django.conf import settings
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     photo = models.ImageField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
